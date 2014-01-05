58a80422334280c9246a63f84c051be3596516ae
drgnak <drgnak@gmail.com>
2014-01-05 00:34:00 -0600

    Initial commit of Unity project with temp assets

---
+++ Assets/Scripts/ValkyrieBoard.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ValkyrieBoard : MonoBehaviour {
+	
+	public Texture2D background;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnGUI () {
+		//GUI.Label(new Rect(0, 0, 1280, 720), new GUIContent(background));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
